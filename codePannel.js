906a5b4bae2f8c8a06e055dbd56bfb9fe428a105
InverseEcho <36345375+InverseEcho@users.noreply.github.com>
2026-05-31 21:26:48 -0400

    Create codePannel.js

---
+++ codePannel.js
@@ -0,0 +1,9 @@
+kv = {import:kv-plugin}
+generateText = {import:ai-text-plugin}
+generateImage = {import:text-to-image-plugin}
+superFetch = {import:super-fetch-plugin}
+$meta
+  title = Luminara
+  description = Luminara. A user drivin AI experience
+  header
+    mode = minimal
