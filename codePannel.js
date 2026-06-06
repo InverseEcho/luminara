5a8eb7d9f02207bc85053e9b910c1a507c61315b
InverseEcho <36345375+InverseEcho@users.noreply.github.com>
2026-06-06 15:22:22 -0400

    Update codePannel.js
    Added something to stop people from mindlessly copying my generator, and a new image that will be the logo

--- codePannel.js
+++ codePannel.js
@@ -2,8 +2,14 @@ kv = {import:kv-plugin}
 generateText = {import:ai-text-plugin}
 generateImage = {import:text-to-image-plugin}
 superFetch = {import:super-fetch-plugin}
+fetcher = {import:yv49wuj5d6}
+
 $meta
+  image = https://user.uploads.dev/file/1940e750f55394f4feaefe92e95250e4.png
   title = Luminara
-  description = Luminara is a new AI for Perchance.
+  description = Luminara - AI Roleplay
   header
     mode = minimal
+    background = light-dark(grey, #0f0f10) // background
+
+// Camoakum
