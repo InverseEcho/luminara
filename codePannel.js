419dbdc5597982d39bb8431ddc0f65462d7ea583
InverseEcho <36345375+InverseEcho@users.noreply.github.com>
2026-06-23 13:34:00 -0400

    Update codePannel.js
    Stable 0.9.0 release

--- codePannel.js
+++ codePannel.js
@@ -7,9 +7,9 @@ fetcher = {import:yv49wuj5d6}
 $meta
   image = https://user.uploads.dev/file/1940e750f55394f4feaefe92e95250e4.png
   title = Luminara
-  description = Luminara - AI Roleplay
+  description = Luminara - AI Roleplay (now with EXTENSIVE memory optimization, chat threads, per-message context viewing, global variables, and much more)
   header
     mode = minimal
     background = light-dark(grey, #0f0f10) // background
 
-// Camoakum
+// Version 0.9.0
